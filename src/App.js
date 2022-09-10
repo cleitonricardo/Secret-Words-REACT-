ca943f7e2d3f7a94d69eb829505db6e12c506eec
Cleiton Ricardo Spagnol <cleitonricardo12@gmail.com>
2022-09-10 13:03:16 -0300

    Feito a estilização do jogo

--- src/App.js
+++ src/App.js
@@ -21,9 +21,38 @@ const stages =[
 function App() {
   const[gameStage, setGameStage] = useState(stages[0].name);
   const[words]= useState(wordsList);
+
+  const [pickedWord, setpickedWord] =useState("");
+  const [pickedCategory, setPickedCategory]= useState("");
+  const [letters, setLetters]= useState("");
   
+  const pickWordAndCategory = ()=> {
+    // pick a random category
+  const categories = Object.keys(words)
+  const category = categories[Math.floor(Math.random()* Object.keys(categories).length)];
+
+  //pick a random word
+
+  const word = words[category][Math.floor(Math.random()* words[category].length)]
+
+    return {word, category}
+  }
+
 //starts the secret word game
   const startGame =() => {
+    //pick word and pick category
+    const {word,category}= pickWordAndCategory();
+
+    //create an array of letters
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    //fill states
+
+    setpickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name)
   };
 
