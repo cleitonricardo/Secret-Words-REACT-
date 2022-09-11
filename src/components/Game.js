9c63fe957e387659f72ed8f73f30d4ed8ac3b5a5
Cleiton Ricardo Spagnol <cleitonricardo12@gmail.com>
2022-09-11 11:06:42 -0300

    Inserção de Letras

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,19 @@
-import "./Game.css"
+import "./Game.css";
+import { useState,useRef } from "react";
 
 const Game = ({verifyLetter, pickedWord, pickedCategory,letters,guessedLetters,wrongLetters, guesses,score,}) => {
-  return (
+  
+const [letter, setLetter]= useState("")
+const letterInputRef = useRef(null);
+
+const handleSubmit = (e) => {e.preventDefault();
+
+verifyLetter(letter)
+setLetter("");
+
+letterInputRef.current.focus();
+}
+    return (
     <div className="game">
         <p className="points">
             <span>Pontuação:{ServiceWorkerRegistration}</span>
@@ -18,8 +30,8 @@ const Game = ({verifyLetter, pickedWord, pickedCategory,letters,guessedLetters,w
         </div>
         <div className="letterContainer">
             <p>Tente advinhar uma letra da palavra:</p>
-            <form>
-                <input type="text" name="letter" maxLength="1" required />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value = {letter} ref={letterInputRef} />
                 <button>Jogar!</button>
             </form>
             <div className="wrongLettersContainer">
